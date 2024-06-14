e2e4e15a3d5e919c92238e14e3c2e0ad326cc6ff
carrillo20221 <137222890+carrillo20221@users.noreply.github.com>
2024-06-14 16:57:03 -0500

    first commit

---
+++ models/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors')
+
+class Server {
+  constructor () {
+    this.app = express()
+    this.port = process.env.PORT
+    this.middleware()
+    this.routes()
+  }
+
+  middleware () {
+    //CORS
+    this.app.unsubscribe(
+      cors()
+    )
+
+    //Lectura y parseo del body
+    this.app.use(
+      express.json()
+    )
+
+    //Directorio Publico
+    this.app.use(
+      express.static('public')
+    )
+  }
+
+  routes () {
+    this.app.use(this.usuariosPath, require('../routes/usuarios'))
+  }
+
+  listen () {
+    this.app.listen(this.port, () => {
+      console.log('Servidor corriendo ', this.port)
+    })
+  }
+}
+
+module.exports = Server
